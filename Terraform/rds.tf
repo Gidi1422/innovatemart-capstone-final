fb90198bf385aa9f78f636c739fd1b0f1d058aa4
Gidi1422 <valentineazagidi101@gmail.com>
2026-02-04 16:34:52 +0100

    FINAL FIX: RDS subnets linked and ghost resources cleared

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -1,12 +1,15 @@
 resource "aws_db_instance" "catalog" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  engine_version       = "15"
-  instance_class       = "db.t3.micro"
-  db_name              = "catalogdb"
-  username             = "dbadmin"
-  password             = "BedrockPass123!" 
-  skip_final_snapshot  = true
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15"
+  instance_class         = "db.t3.micro"
+  db_name                = "catalogdb"
+  username               = "dbadmin"
+  password               = "BedrockPass123!" 
+  skip_final_snapshot    = true
+  
+  # FIX: This links the DB to your VPC subnets
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   
   tags = { Project = "Bedrock" }
@@ -15,10 +18,19 @@ resource "aws_db_instance" "catalog" {
 resource "aws_security_group" "rds_sg" {
   name   = "project-bedrock-rds-sg"
   vpc_id = module.vpc.vpc_id
+
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    # Allows access from within your VPC
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
